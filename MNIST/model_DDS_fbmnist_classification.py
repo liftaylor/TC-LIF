1cc8618338985cf6c75af44a624af021fa7da6ef
Xiaowei <thundertail@gmail.com>
2025-06-22 15:34:46 +0100

    180 Epochs so far, 94.55% accuracy

--- MNIST/model_DDS_fbmnist_classification.py
+++ MNIST/model_DDS_fbmnist_classification.py
@@ -19,7 +19,7 @@ def set_seed(seed=42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.benchmark = True
     os.environ["PYTHONHASHSEED"] = str(seed)
 
 
